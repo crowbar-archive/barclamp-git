5e7dae7beeec95e2b5ac0643c4f2556b623c818c
Evgeniy Shurmin <eshurmin@gmai.com>
2013-03-06 14:10:52 +0400

    fixes

--- chef/cookbooks/git/definitions/pfs_and_install_deps.rb
+++ chef/cookbooks/git/definitions/pfs_and_install_deps.rb
@@ -1,10 +1,7 @@
-require 'find'
-
-define :pfs_and_install_deps, :action => :create do
+define :pfs_and_install_deps, :action => :create, :virtualenv => nil do
   #params:
   #  name: name of component to be installed in pull-from-source mode
-  #  venv: path where will be created python virtualenv
-  #  venv_bins: binaries to wrapping form package to /usr/local/bin
+  #  virtualenv: install using virtualenv if true
   #  path: path on the node's filesystem to clone git repo to [default: /opt/#{comp_name} ]
   #  cookbook: name of cookbook to use for pull-from-source [default: current cookbook ]
   #  cnode: node where all the pull-from-source attributes related to the current proposal are [default: current node ]
@@ -29,34 +26,11 @@ define :pfs_and_install_deps, :action => :create do
   install_path = params[:path] || "/opt/#{comp_name}"
   cbook = params[:cookbook] || @cookbook_name
   cnode = params[:cnode] || node
-  ref = params[:reference] || cnode[cbook][:git_refspec] 
+  ref = params[:reference] || cnode[cbook][:git_refspec]
 
   package("git")
   package("python-setuptools")
   package("python-pip")
-  
-  params[:venv] ||= ""
-  params[:venv_bins] ||= []
-
-  prefix = ""
-
-  # creatinv virtualenv if params[:venv] present
-  unless params[:venv].empty?
-    package("python-virtualenv")
-    package("python-dev")
-    prefix = params[:venv] + "/bin/"
-    directory params[:venv] do
-      recursive true
-      owner "root"
-      group "root"
-      mode  0775
-      action :create
-    end
-    execute "create virtualenv" do    
-      command "virtualenv #{params[:venv]} --system-site-packages"
-      not_if "test -e #{params[:venv]}/bin/python"
-    end
-  end
 
   if cnode[cbook][:use_gitbarclamp]
     env_filter = " AND git_config_environment:git-config-#{cnode[cbook][:git_instance]}"
@@ -66,19 +40,37 @@ define :pfs_and_install_deps, :action => :create do
     git_url = cnode[cbook][:gitrepo]
   end
 
+  git install_path do
+    repository git_url
+    reference ref
+    action :sync
+  end
+
+  # prepare prefix to commands in using virtualenv
+  prefix = params[:virtualenv] ? ". #{params[:virtualenv]}/bin/activate && " : nil
+
+  if params[:virtualenv] and not File.exist?(params[:virtualenv])
+    # prefix not nil and .venv not exist try create virtualenv
+    package("python-virtualenv")
+    package("python-dev")
+    directory params[:virtualenv] do
+      recursive true
+      owner "root"
+      group "root"
+      mode  0775
+      action :create
+    end
+    execute "virtualenv #{params[:virtualenv]} --system-site-packages"
+  end
+
+  pip_cmd = "#{prefix}pip install"
   if cnode[cbook][:use_pip_cache]
     provisioner = search(:node, "roles:provisioner-server").first
     proxy_addr = provisioner[:fqdn]
     proxy_port = provisioner[:provisioner][:web_port]
     pip_cmd = "#{prefix}pip install --index-url http://#{proxy_addr}:#{proxy_port}/files/pip_cache/simple/"
-  else
-    pip_cmd = "#{prefix}pip install"
   end
-  git install_path do
-    repository git_url 
-    reference ref
-    action :sync
-  end
+
   if cnode[comp_name]
     unless cnode[comp_name][:pfs_deps].nil?
       deps = cnode[comp_name][:pfs_deps].dup
@@ -96,13 +88,14 @@ define :pfs_and_install_deps, :action => :create do
           version pkg_version if pkg_version != pkg
         end
       end
-      (pip_deps - pip_pythonclients).each do |pkg| 
+      (pip_deps - pip_pythonclients).each do |pkg|
         execute "pip_install_#{pkg}" do
           command "#{pip_cmd} '#{pkg}'"
         end
       end
     end
   end
+
   unless params[:without_setup]
     # workaround for swift
     execute "remove_https_from_pip_requires_for_#{comp_name}" do
@@ -120,34 +113,11 @@ define :pfs_and_install_deps, :action => :create do
       creates "#{install_path}/#{comp_name == "nova_dashboard" ? "horizon":comp_name}.egg-info"
     end
     # post install clients
-    pip_pythonclients.each do |pkg| 
+    pip_pythonclients.each do |pkg|
       execute "pip_install_clients_#{pkg}_for_#{comp_name}" do
         command "#{pip_cmd} '#{pkg}'"
       end
     end
   end
-
-  # creatinv virtualenv if params[:venv] present
-  unless params[:venv].empty? and params[:venv_bins].empty?
-    from = File.join(params[:venv],"bin")
-    to = "/usr/local/bin"
-    bins = params[:venv_bins].to_a.collect{|e| e.to_s}
-    bins.each  do |file|
-      bin_name = file.split("/").last
-      template "#{to}/#{bin_name}" do
-        cookbook "git"
-        source "virtualenv.erb"
-        mode 0755
-        owner "root"
-        group "root"
-        variables({
-          :venv => params[:venv],
-          :original_bin => "#{install_path}/bin/#{bin_name}",
-        })
-      end
-    end
-
-  end
-
 end
 
