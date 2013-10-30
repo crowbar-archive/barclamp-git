6de9cd81a8c9543a825f9719eb5e5e2af638c098
Evgeniy Shurmin <eshurmin@gmail.com>
2013-10-30 16:58:19 +0300

    create group with member

--- chef/cookbooks/git/definitions/create_user_and_dirs.rb
+++ chef/cookbooks/git/definitions/create_user_and_dirs.rb
@@ -1,7 +1,7 @@
-define :create_user_and_dirs, :action => :enable do
-
+define :create_user_and_dirs, :action => :enable, :user_name => nil, :group_name => nil do
   # params:
   #   user_name - name of the user to create  [default: name]
+  #   group_name - name of the group to create  [default: name]
   #   comp_name - name of component to deploy (eg. glance) [default: name]
   #   home_dir - home directory for the user [default: /var/lib/#{comp_name}]
   #   user_gid - existing group id for the user [default: nil] 
@@ -10,6 +10,7 @@ define :create_user_and_dirs, :action => :enable do
   #   
 
   user_name = params[:user_name] || params[:name]
+  group_name = params[:group_name] || params[:name]
   comp_name = params[:comp_name] || params[:name]
   dir_group = params[:dir_group] || "root"
   dirs = ["/var/lib", "/var/log", "/var/lock", "/etc"]
@@ -23,7 +24,12 @@ define :create_user_and_dirs, :action => :enable do
     system true
     shell "/bin/false"
   end
-  
+ 
+  group group_name do
+    members user_name
+    system true
+  end
+ 
   dirs.each do |d|
     directory d do
       owner user_name
