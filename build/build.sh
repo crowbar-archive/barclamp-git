ed81e68434dbf06a9e2fc13f49e44609ad577e27
Aleksey Shcherba <ashcherba@mirantis.com>
2014-06-17 07:51:36 +0300

    Fix cache git tracking in PFS build script

--- build/build.sh
+++ build/build.sh
@@ -8,4 +8,8 @@ bc_needs_build() {
 bc_build() {
     sudo pip install pip2pi
     $BC_DIR/build/build.rb
+    if [[ $CURRENT_CACHE_BRANCH ]]; then
+        CACHE_NEEDS_COMMIT=true
+        in_cache git add -A "barclamps/git/files/"
+    fi
 }
